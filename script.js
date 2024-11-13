9922b8fb022370aa0c0100f47a2c5b84338f655f
ailsapm <alimooney2013@gmail.com>
2024-11-13 03:26:56 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//define vars
+var rockB = document.getElementById('rock');
+var paperB = document.getElementById('paper');
+var scissorsB = document.getElementById('scissors');
+//recognise player's pick
+rockB.addEventListener('click',()=> takeTurn('rockB'));
+paperB.addEventListener('click',()=> takeTurn('paperB'));
+scissorsB.addEventListener('click',()=> takeTurn('scissorsB'));
+//game rules
+function takeTurn (pick) {
+    var compPick = [Math.floor(Math.random()*3)];
+    //who wins
+    if ((pick ==='rockB' && compPick==0) || (pick==='paperB' && compPick==1) || (pick==='scissorsB' && compPick==2)) {
+        alert ("It's a draw!");
+    } 
+    else if ((pick ==='rockB' && compPick==1) || (pick==='paperB' && compPick==2) || (pick==='scissorsB' && compPick==0)) {
+        alert ("You lose!");
+    } 
+    else {
+            alert ("You win!");
+    }
+}
